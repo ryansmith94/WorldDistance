0b02d8691fbadec7ed481d7ebc47826f5d677e59
Ryan Smith <0ryansmith1994@gmail.com>
2013-10-09 17:29:34 +0100

    Wrote the boilerplate using class designs.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,105 @@
-#include <iostream>
-using namespace <std>
+#import <iostream>
+#import <string>
+using namespace std;
+
+template <class OptionType>
+class Options {
+		OptionType options[];
+	public:
+		Options display() {
+
+		}
+		OptionType getOption() {
+
+		}
+		OptionType* getOptions() {
+
+		}
+};
+
+class Place {
+		string name;
+		float longitude;
+		float latitude;
+		Place* parent;
+		Place* children[];
+	public:
+		string getName() {
+			return 0;
+		}
+		float getLongitude() {
+			return 0;
+		}
+		float getLatitude() {
+			return 0;
+		}
+		Place* setName(string newName) {
+			return new Place();
+		}
+		Place* getParent() {
+			return new Place();
+		}
+		Place* getChild() {
+			return new Place();
+		}
+		int getNbrChildren() {
+			return 0;
+		}
+		Place* addChild(Place newPlace) {
+			return new Place();
+		}
+};
+
+class PlacesController {
+		Place places[];
+	public:
+		Place* getPlaces(string name, Place parent) {
+			return 0;
+		}
+		PlacesController* addPlace(Place newPlace) {
+			return this;
+		}
+		PlacesController* loadPlaces(string dataLocation) {
+			return this;
+		}
+		PlacesController* savePlaces(string dataLocation) {
+			return this;
+		}
+};
+
+class DistanceApp {
+		PlacesController* places;
+	public:
+		DistanceApp* start() {
+			return this;
+		}
+};
+
+class DistanceView {
+		PlacesController* places;
+	public:
+		DistanceView* start() {
+			return this;
+		}
+};
+
+class PlacesView {
+		PlacesController* places;
+	public:
+		PlacesView* start() {
+			return this;
+		}
+};
+
+class AddPlacesView {
+		PlacesController* places;
+	public:
+		AddPlacesView* start() {
+			return this;
+		}
+};
 
 int main() {
-    return 0;
+	cout << "I work... for now >;)" << endl;
+	return 0;
 }
