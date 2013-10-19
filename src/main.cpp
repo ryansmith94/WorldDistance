4c2d3194d88937ae6c67f5e8f5fbc8c674c7cd04
Ryan Smith <0ryansmith1994@gmail.com>
2013-10-19 19:02:44 +0100

    Included core.cpp.

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,6 @@
-#include <iostream>
-#include <string>
+#include "core.cpp"
 using namespace std;
 
 int main() {
-	cout << "I work... for now >;)" << endl;
-
-	// Demonstration of OptionsView
-	cout << endl << "Just testing the OptionsView class ;)" << endl;
-	string opts[] = {"Option 1", "Option 2", "Quit"};
-	OptionsView<string>* o = new OptionsView<string>(opts, sizeof(opts) / sizeof(string));
-	cout << o->display()->getOption() << endl; // Notice that you can chain calls `display()->getOption()`
-
 	return 0;
 }
