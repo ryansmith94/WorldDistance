32efc83593b515f55534663cc50dbe407d40e4c9
Blind01 <parkeroliver02@yahoo.com>
2013-10-21 22:43:25 +0100

    added Linked List class as seperate file for now

---
+++ src/LinkedList.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+template <class T>
+class LinkedList {
+	private:
+		T *data;
+		LinkedList *next;
+		
+		T getData(){
+			return *data;
+		}
+		T* getDataRef(){
+			return data;
+		}
+		LinkedList* getNext(){
+			return next;
+		}
+	public:
+		LinkedList(){
+			data = NULL;
+			next = NULL;
+		}
+		LinkedList (T nextData){
+			data = new T(nextData);
+			next = NULL;
+		}
+		LinkedList (T *nextData){
+			data = nextData;
+			next = NULL;
+		}
+		~LinkedList(){
+			delete data;
+			delete next;
+		}
+		void append(T nextData){
+			if (data == NULL) data = new T(nextData);
+			else {
+				if (next == NULL) next = new LinkedList(nextData);
+				else next->append(nextData);
+			}		
+		}
+		void append(T *nextData){
+			if (data == NULL) data = nextData;
+			else{
+				if (next == NULL) next = new LinkedList(*nextData);
+				else next->append(*nextData);
+			}
+		}
+		int getLength(){
+			int result;
+			if (data == NULL) result = 0;
+			else result = 1;
+			if (next != NULL) result += next->getLength();
+			return result;
+		}
+		T getData(int index){
+			T result;
+			if (index) result = getNext()->getData(--index);
+			else result = *data;
+			return result;
+		}
+		T* getDataRef(int index){
+			T *result;
+			if (index) result = getNext()->getDataRef(--index);
+			else result = data;
+			return result;
+		}
+		void remove(int index){
+			//special case for end of list(remove last element)
+			if (index == 1 && next->next == NULL){
+				delete next;
+				next = NULL;
+			}
+			//recurse if not at element to remove
+			else if (index) getNext()->remove(--index);
+			//special case for first element is last element(cant remove first link)
+			else if (!index && next == NULL){
+				delete data;
+				data = NULL;
+			}
+			//normal case
+			else{
+				LinkedList *temp = next;
+				delete data;
+				data = temp->data;
+				temp->data = NULL;
+				next = temp->next;
+				temp->next = NULL;
+				delete temp;
+			}
+		}
+};
+
+int main(){
+	LinkedList<string> *myList = new LinkedList<string>();
+	myList->append("Hello");
+	myList->append("World");
+	myList->append("Cheese");
+	myList->remove(0);
+	myList->remove(0);
+	myList->remove(0);
+	for(int i = 0; i<myList->getLength();i++){
+		cout<<myList->getData(i)<<endl;
+	}
+	myList->append("dog");
+	myList->append("cat");
+	for(int i = 0; i<myList->getLength();i++){
+		cout<<myList->getData(i)<<endl;
+	}
+	return 1;
+}
