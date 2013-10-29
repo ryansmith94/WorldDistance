9e8b74c0040a66da198abd4029e80459ea28ecbe
H. Bristow <harleybristowabc@googlemail.com>
2013-10-29 00:31:49 +0000

    Update of branch with newer code.

--- src/core.cpp
+++ src/core.cpp
@@ -1,187 +1,246 @@
 #include <iostream>
 #include <string>
+#include <cmath>
 using namespace std;
 
 template <class T>
-class LinkedList {
-        private:
-                T *data;
-                LinkedList *next;
-                
-                T getData(){
-                        return *data;
-                }
-                T* getDataRef(){
-                        return data;
-                }
-                LinkedList* getNext(){
-                        return next;
-                }
-        public:
-                LinkedList(){
-                        data = NULL;
-                        next = NULL;
-                }
-                LinkedList (T nextData){
-                        data = new T(nextData);
-                        next = NULL;
-                }
-                LinkedList (T *nextData){
-                        data = nextData;
-                        next = NULL;
-                }
-                ~LinkedList(){
-                        delete data;
-                        delete next;
-                }
-                void append(T nextData){
-                        if (data == NULL) data = new T(nextData);
-                        else {
-                                if (next == NULL) next = new LinkedList(nextData);
-                                else next->append(nextData);
-                        }                
-                }
-                void append(T *nextData){
-                        if (data == NULL) data = nextData;
-                        else{
-                                if (next == NULL) next = new LinkedList(*nextData);
-                                else next->append(*nextData);
-                        }
-                }
-                int getLength(){
-                        int result;
-                        if (data == NULL) result = 0;
-                        else result = 1;
-                        if (next != NULL) result += next->getLength();
-                        return result;
-                }
-                T getData(int index){
-                        T result;
-                        if (index) result = getNext()->getData(--index);
-                        else result = *data;
-                        return result;
-                }
-                T* getDataRef(int index){
-                        T *result;
-                        if (index) result = getNext()->getDataRef(--index);
-                        else result = data;
-                        return result;
-                }
-                void insert(T newData, int index){
-                        if(index == 1 && next == NULL){
-                                next = new LinkedList(newData);
-                                next->next = NULL;
-                        }
-                        else if(index) getNext()->insert(newData,--index);
-                        else{
-                                LinkedList *newNext = new LinkedList(data);
-                                newNext->next = next;
-                                data = new T(newData);
-                                next = newNext;
-                        }
-                }
-                void insert(T *newData, int index){
-                        if(index == 1 && next ==NULL){
-                                next = new LinkedList(newData);
-                                next->next = NULL;
-                        }
-                        else if(index) getNext()->insert(newData,--index);
-                        else{
-                                LinkedList *newNext = new LinkedList(data);
-                                newNext->next = next;
-                                data = newData;
-                                next = newNext;
-                        }
-                                
-                }
-                void remove(int index){
-                        //special case for end of list(remove last element)
-                        if (index == 1 && next->next == NULL){
-                                delete next;
-                                next = NULL;
-                        }
-                        //recurse if not at element to remove
-                        else if (index) getNext()->remove(--index);
-                        //special case for first element is last element(cant remove first link)
-                        else if (!index && next == NULL){
-                                delete data;
-                                data = NULL;
-                        }
-                        //normal case
-                        else{
-                                LinkedList *temp = next;
-                                delete data;
-                                data = temp->data;
-                                temp->data = NULL;
-                                next = temp->next;
-                                temp->next = NULL;
-                                delete temp;
-                        }
-                }
+class Node{
+	private:
+		T *data;
+		Node *next;
+	public:
+		Node(T*,Node*);
+		Node(T*);
+		Node();
+		~Node();
+		Node* getNext();
+		void setNext(Node*);
+		T* getData();
+		void setData(T*);
+};
+template <class T>
+class LList{
+	private:
+		int size;
+		Node<T> *first;
+	public:
+		LList();
+		~LList();
+		int getSize();
+		void append(T*);
+		void insert(T*,int);
+		void remove(int);
+		T* getData(int);	
+		void addAll(LList<T>*);
+		void clear();
 };
 
+template <class T>
+Node<T>::Node(T *newData, Node *nextNode){
+	data = newData;
+	next = nextNode;
+}
+template <class T>
+Node<T>::Node(T *newData){
+	data = newData;
+	next = NULL;
+}
+template <class T>
+Node<T>::Node(){
+	data = new T();
+	next = NULL;
+}
+template <class T>
+Node<T>::~Node(){
+	delete next;
+	delete data;
+}
+template <class T>
+Node<T>* Node<T>::getNext(){
+	return next;
+}
+template <class T>
+void Node<T>::setNext(Node *nextNode){
+	next = nextNode;
+}
+template <class T>
+T* Node<T>::getData(){
+	return data;
+}
+template <class T>
+void Node<T>::setData(T *newData){
+	data = newData;
+}
+
+template <class T>
+LList<T>::LList(){
+	size = 0;
+	first = NULL;
+}
+template <class T>
+LList<T>::~LList(){
+	delete first;
+}
+template <class T>
+int LList<T>::getSize(){
+	return size;
+}
+template <class T>
+void LList<T>::append(T *newData){
+	if (first == NULL){
+		first = new Node<T>(newData);
+	}
+	else{
+		Node<T> *node = first;
+		while(node->getNext() != NULL){
+			node = node->getNext();
+		}
+		node->setNext(new Node<T>(newData));
+	}
+	size++;
+}
+template <class T>
+void LList<T>::insert(T *newData,int index){
+	if (!index){
+		Node<T> *newNode = new Node<T>(newData,first);
+		first = newNode;
+	}
+	else if(index == size){
+		this->append(newData);
+		size--;
+	}
+	else{
+		Node<T> *node = first;
+		for (int i=0;i<index-1;i++){
+			node = node->getNext();
+		}
+		Node<T> *newNode = new Node<T>(newData,node->getNext());
+		node->setNext(newNode);
+	}
+	size++;
+}
+template <class T>
+void LList<T>::remove(int index){
+	if (!index){
+		Node<T> *temp = first;
+		first = first->getNext();
+		temp->setNext(NULL);
+		delete temp;
+	}
+	else{
+		Node<T> *node = first;
+		for (int i=0;i<index-1;i++){
+			node = node->getNext();
+		}
+		if (node->getNext()->getNext() == NULL){
+			delete node->getNext();
+			node->setNext(NULL);
+		}
+		else{
+			Node<T> *temp = node->getNext();
+			node->setNext(temp->getNext());
+			temp->setNext(NULL);
+			delete temp;
+		}
+	}
+	size--;
+}
+template <class T>
+T* LList<T>::getData(int index){
+	Node<T> *node = first;
+	for (int i=0;i<index;i++){
+		node = node->getNext();
+	}
+	return node->getData();
+}
+template <class T>
+void LList<T>::addAll(LList<T> *list){
+	for (int i=0;i<list->getSize();i++){
+		append(list->getData(i));
+	}
+}
+template <class T>
+void LList<T>::clear(){
+	if (first!=NULL){
+		Node<T> *temp = first;
+		for (int i=0;i<size;i++){
+			temp->setData(NULL);
+			temp = temp->getNext();
+		}
+		delete first;
+		first = NULL;
+		size = 0;
+	}
+}
 
 template <class OptionType>
 class OptionsView {
-		OptionType *options;
-		int numberOfOptions;
-	public:
-		OptionsView (OptionType *opts, int len = 0) {
-			options = opts;
-			numberOfOptions = len;
-		}
-		~OptionsView () {
+        OptionType *options;
+        int numberOfOptions;
+    public:
+        OptionsView (OptionType *opts, int len = 0) {
+            options = opts;
+            numberOfOptions = len;
+        }
+        ~OptionsView () {
 
-		}
-		OptionsView* display() {
-			for (int i = 0; i < numberOfOptions; i += 1) {
-				cout << i + 1 << ". " << options[i] << endl;
-			}
-			cout << endl;
-			return this;
-		}
-		OptionType getOption() {
-			int selected = -1;
+        }
+        OptionsView* display() {
+            for (int i = 0; i < numberOfOptions; i += 1) {
+                cout << i + 1 << ". " << options[i] << endl;
+            }
+            cout << endl;
+            return this;
+        }
+        OptionType getOption() {
+            int selected = -1;
 
-			do {
-				cout << "Please select an option (1-" << numberOfOptions <<  "): ";
-				cin >> selected;
-			} while (selected < 1 || selected > numberOfOptions);
+            do {
+                cout << "Please select an option (1-" << numberOfOptions <<  "): ";
+                cin >> selected;
+            } while (selected < 1 || selected > numberOfOptions);
 
-			return options[selected - 1];
-		}
-		OptionType* getOptions() {
-			return options;
-		}
+            return options[selected - 1];
+        }
+        OptionType* getOptions() {
+            return options;
+        }
 };
 
 class Place {
+	private:
 		string name;
 		float longitude;
 		float latitude;
 		Place* parent;
-		LinkedList<Place>* children;
+		LList<Place> children;
+		string address;
+		LList<Place> *matches;
+		
+		void setAddress(){
+			string newAddress = name;
+			if (parent!=NULL){
+				newAddress = parent->getAddress() + ", " + newAddress;
+			}
+			address = newAddress;
+			for (int i=0;i<children.getSize();i++){
+				children.getData(i)->setAddress();
+			}
+		}
+		
 	public:
 		Place(string newName, float newLongitude, float newLatitude, Place* newParent) {
 			name = newName;
 			longitude = newLongitude;
 			latitude = newLatitude;
 			parent = newParent;
-			children = new LinkedList<Place>();
-		}
-
-		/* Super Parent constructor */
-		Place(string newName, float newLongitude, float newLatitude) {
-			name = newName;
-			longitude = newLongitude;
-			latitude = newLatitude;
-			//parent = earth ?
-			children = new LinkedList<Place>();
+			setAddress();
+			matches = new LList<Place>();
 		}
 		
 		~Place() {
-
-			delete children;
+			matches->clear();
+			delete matches;
 		}
 		
 		string getName() {
@@ -197,8 +256,8 @@ class Place {
 		}
 		
 		Place* setName(string newName) {
-			/* name = newName	*/
 			this->name = newName;
+			setAddress();
 			return this;
 		}
 		
@@ -206,31 +265,34 @@ class Place {
 			return parent;
 		}
 		
-		Place* getChildren(){
-			/* Should this return an array/list? 		*/
-			return this;
+		LList<Place>* getMatchedChildren(string searchTerm){
+			matches->clear();
+			if (name.find(searchTerm)!=string::npos||(searchTerm.find(name)!=string::npos && address.find(searchTerm)!=string::npos)){
+				matches->append(this);
+			}
+			for(int i=0;i<children.getSize();i++){
+				matches->addAll(children.getData(i)->getMatchedChildren(searchTerm));
+			}
+			return matches;
 		}
 		
 		Place* getChild(int index) {
-			if (!getNbrChildren()) return NULL;
-			else return children->getDataRef(index);
+			return children.getData(index);
 		}
 		
 		int getNbrChildren() {
-			/* 	sizeof(array) / sizeof(one_element) = number of elements in array	*/
-					
-			//return ((int)(&children+1)-(int)children)/(sizeof(children[0]));
-			return children->getLength();
+			return children.getSize();
 		}
 		
-		void addChild(Place newPlace) {
-			cout<<"appending newPlace..."<<endl;cin.get();
-			children->append(newPlace);
-			cout<<"done"<<endl;cin.get();
+		Place* addChild(Place* newPlace) {
+			newPlace->parent = this;
+			children.append(newPlace);
+			children.getData(children.getSize()-1)->setAddress();
+			return this;
 		}
 		
 		Place* removeChild(int index){
-			children->remove(index);
+			children.remove(index);
 			return this;
 		}
 		
@@ -241,126 +303,127 @@ class Place {
 		Place* savePlace(string datalocation){
 			return this;
 		}
+		
+		string getAddress(){
+			return address;
+		}
+		
 
+};
+
+class View {
+    protected:
+        Place* rootPlace;
+        string getAddress(int rejectEmpty = 0) {
+            string address;
+            do {
+                cout << "Please enter an address: ";
+                cin >> address;
+            } while (rejectEmpty == 1 && address == "");
+            return address;
+        }
+    public:
+        View(Place* place) {
+            rootPlace = place;
+        }
+        virtual View* start() =0;
+};
+
+class DistanceView: public View {
+        double calculateDistance(Place* from, Place* to) {
+            // Using the haversine forumla.
+            double radius = 6371;
+            double lat1 = from->getLatitude();
+            double lat2 = to->getLatitude();
+            double dLat = (lat2 - lat1) * (M_PI / 180);
+            double dLon = (to->getLongitude() - from->getLongitude()) * (M_PI / 180);
+            double a;
+
+            lat1 = lat1 * (M_PI / 180);
+            lat2 = lat2 * (M_PI / 180);
+
+            a = sin(dLat / 2) * sin(dLat / 2) + sin(dLon / 2) * sin(dLon / 2) * cos(lat1) * cos(lat2);
+            
+            return radius * (2 * atan2(sqrt(a), sqrt(1 - a)));
+        }
+    public:
+        DistanceView(Place* place) : View(place) {}
+        DistanceView* start() {
+            string address1;
+            string address2;
+            address1 = getAddress(1);
+            address2 = getAddress(1);
+            // Calculate distance.
+            return this;
+        }
+};
+
+class PlacesView: public View {
+    public:
+        PlacesView(Place* place) : View(place) {}
+        PlacesView* start() {
+            return this;
+        }
+};
+
+class AddPlacesView: public View {
+    public:
+        AddPlacesView(Place* place) : View(place) {}
+        AddPlacesView* start() {
+            return this;
+        }
+};
+
+class ModifyPlacesView: public View {
+    public:
+        ModifyPlacesView(Place* place) : View(place) {}
+        ModifyPlacesView* start() {
+            return this;
+        }
+};
+
+class DeletePlacesView: public View {
+    public:
+        DeletePlacesView(Place* place) : View(place) {}
+        DeletePlacesView* start() {
+            return this;
+        }
 };
 
 class DistanceApp {
-		Place* place;
-	public:
-		DistanceApp() {
+        Place* rootPlace;
+        string dataLocation;
+    public:
+        DistanceApp() {
+            dataLocation = "data.txt";
+            rootPlace = new Place("Earth", 0.0, 0.0, NULL);
+        }
+        DistanceApp* start() {
+            string opts[6] = {"Distance", "Places", "Add place", "Modify place", "Delete place", "Quit"};
+            OptionsView<string> optionsView(opts, 6);
+            DistanceView distanceView(rootPlace);
+            PlacesView placesView(rootPlace);
+            AddPlacesView addPlacesView(rootPlace);
+            ModifyPlacesView modifyPlacesView(rootPlace);
+            DeletePlacesView deletePlacesView(rootPlace);
+            string selected;
 
-		}
-		~DistanceApp() {
+            do {
+                selected = optionsView.display()->getOption();
 
-		}
-		DistanceApp* start() {
-			return this;
-		}
+                if (selected == opts[0]) {
+                    distanceView.start();
+                } else if (selected == opts[1]) {
+                    placesView.start();
+                } else if (selected == opts[2]) {
+                    addPlacesView.start();
+                } else if (selected == opts[3]) {
+                    modifyPlacesView.start();
+                } else if (selected == opts[4]) {
+                    deletePlacesView.start();
+                }
+            } while (selected != "Quit");
+
+            return this;
+        }
 };
-
-class DistanceView {
-		Place* place;
-	public:
-		DistanceView(Place* place) {
-
-		}
-		~DistanceView() {
-
-		}
-		DistanceView* start() {
-			return this;
-		}
-};
-
-class PlacesView {
-		Place* place;
-	public:
-		PlacesView(Place* newPlace) {
-			place = newPlace;
-		}
-		~PlacesView() {
-
-		}
-		PlacesView* start() {
-			int r = place->getNbrChildren();
-
-			cout << r << endl;
-			
-			for (int i=0; i < r; i++){
-				Place* child = place->getChild(i);
-				cout << "Name: " << child->getName() <<
-					"\t| " << child->getLongitude() <<
-					"\t| " << child->getLatitude() << endl;
-			}
-			
-			return this;
-		}
-};
-
-class AddPlacesView {
-		Place* place;
-	public:
-		AddPlacesView(Place* place) {
-
-		}
-		~AddPlacesView() {
-
-		}
-		AddPlacesView* start() {
-
-			
-			return this;
-		}
-};
-
-class ModifyPlacesView {
-		Place* places;
-	public:
-		ModifyPlacesView(Place* newPlace) {
-
-		}
-		~ModifyPlacesView() {
-
-		}
-		ModifyPlacesView* start() {
-			return this;
-		}
-};
-
-class DeletePlacesView {
-		Place* place;
-	public:
-		DeletePlacesView(Place* place) {
-
-		}
-		~DeletePlacesView() {
-
-		}
-		DeletePlacesView* start() {
-			return this;
-		}
-};
-
-
-
-/* Only a temp main */
-int main() {
-	Place *Austria = 	new Place("Austria",	14.301758,48.283787);
-	
-	Place *Vienna = 	new Place("Vienna",	16.400146,48.210627,Austria);
-	Place *Linz = 		new Place("Linz",		14.312744,48.327633,Austria);
-	Place *Salzburg = 	new Place("Salzburg",	13.047256,47.808221,Austria);
-
-	// addChild doesnt do anything yet!
-	cout<<"Now on Vienna..."<<endl;cin.get();
-	Austria->addChild(*Vienna);
-	cout<<"Now on Linz..."<<endl;cin.get();
-	Austria->addChild(*Linz);
-	Austria->addChild(*Salzburg);
-
-	PlacesView con = PlacesView(Austria);
-
-	con.start();
-	
-	return 0;
-}
