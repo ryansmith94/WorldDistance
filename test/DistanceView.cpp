dcb9ab297f4e4952b68ed8e84f43682415620aaf
Ryan Smith <0ryansmith1994@gmail.com>
2013-11-09 19:03:54 +0000

    Updated tests for DistanceView.

--- test/DistanceView.cpp
+++ test/DistanceView.cpp
@@ -18,17 +18,17 @@ Describe(DistanceView_class) {
             TestWithStdIO ioTest("../test/DistanceView/validInput1.txt", TMP_OUT);
             Place place("name", 0, 0);
             place.addChild(new Place("place1", 0.0, 0.0));
-            DistanceView view(&place);
+            DistanceView view(&place, new HashTable());
             Assert::That(view.start(), Equals(&view));
         }
         It(should_accept_two_valid_places) {
-			{
-				TestWithStdIO ioTest("../test/DistanceView/validInput1.txt", TMP_OUT);
-				Place place("name", 0, 0);
-				place.addChild(new Place("place1", 0.0, 0.0));
-				DistanceView view(&place);
-				view.start();
-			}
+            {
+             TestWithStdIO ioTest("../test/DistanceView/validInput1.txt", TMP_OUT);
+             Place place("name", 0, 0);
+             place.addChild(new Place("place1", 0.0, 0.0));
+             DistanceView view(&place, new HashTable());
+             view.start();
+            }
             Assert::That(compareFiles(TMP_OUT, "../test/DistanceView/validOutput1.txt"), Equals(1));
         }
         It(should_accept_two_valid_places_of_same_name) {
@@ -39,30 +39,30 @@ Describe(DistanceView_class) {
                 place.addChild(place1);
                 place.addChild(new Place("place2", 0.0, 0.0));
                 place1->addChild(new Place("place2", 0.0, 0.0));
-                DistanceView view(&place);
+                DistanceView view(&place, new HashTable());
                 view.start();
             }
             Assert::That(compareFiles(TMP_OUT, "../test/DistanceView/validOutput2.txt"), Equals(1));
         }
         It(should_reject_an_invalid_address) {
-			{
-				TestWithStdIO ioTest("../test/DistanceView/invalidInput1.txt", TMP_OUT);
-				Place place("name", 0, 0);
-				place.addChild(new Place("place1", 0.0, 0.0));
-				DistanceView view(&place);
-				view.start();
-			}
+            {
+             TestWithStdIO ioTest("../test/DistanceView/invalidInput1.txt", TMP_OUT);
+             Place place("name", 0, 0);
+             place.addChild(new Place("place1", 0.0, 0.0));
+             DistanceView view(&place, new HashTable());
+             view.start();
+            }
             Assert::That(compareFiles(TMP_OUT, "../test/DistanceView/invalidOutput1.txt"), Equals(1));
         }
         It(should_output_the_correct_distance_between_the_two_places) {
-			{
-				TestWithStdIO ioTest("../test/DistanceView/validInput3.txt", TMP_OUT);
-				Place place("name", 0, 0);
-				place.addChild(new Place("London", 0.1275, 51.5072));
-				place.addChild(new Place("New York", 74.0060, 40.7144));
-				DistanceView view(&place);
-				view.start();
-			}
+            {
+             TestWithStdIO ioTest("../test/DistanceView/validInput3.txt", TMP_OUT);
+             Place place("name", 0, 0);
+             place.addChild(new Place("London", 0.1275, 51.5072));
+             place.addChild(new Place("New York", 74.0060, 40.7144));
+             DistanceView view(&place, new HashTable());
+             view.start();
+            }
             Assert::That(compareFiles(TMP_OUT, "../test/DistanceView/validOutput3.txt"), Equals(1));
         }
     };
