0d0b550e64e2ae56b7ea851d3537273017c27784
Ryan Smith <0ryansmith1994@gmail.com>
2013-11-09 12:27:23 +0000

    HashTable class (partly working - see extended description)
    Getting a segmentation fault when searching for an address that matches
    many Places.

---
+++ src/HashTable.cpp
@@ -0,0 +1,122 @@
+#ifndef HASHTABLE_H
+#define HASHTABLE_H
+#include <string>
+#include "Place.cpp"
+#include "Node.cpp"
+#define A 97
+#define Z 122
+#define SPACE 32
+#define NUL 0
+#define COMMA 44
+
+class HashTable {
+    private:
+        Place* placeTable[29];
+        HashTable* hashTable[29];
+        int index;
+
+        int hash(string value) {
+            int asciiChar = value[index];
+
+            if (asciiChar >= A && asciiChar <= Z) {
+                return asciiChar - A;
+            } else if (asciiChar == SPACE) {
+                return 26;
+            } else if (asciiChar == NUL) {
+                return 27;
+            } else if (asciiChar == COMMA) {
+                return 28;
+            }
+
+            return 0;
+        }
+        // Return the hashtable that the place is added to.
+        HashTable* addPlace(string address, Place* place) {
+            int key = hash(address);
+
+            // Add to hashtable.
+            if (placeTable[key] != NULL) {
+                if (address == placeTable[key]->getAddress()) {
+                    return NULL;
+                }
+                HashTable* ht = new HashTable(index + 1);
+                hashTable[key] = ht;
+                ht->addPlace(placeTable[key]->getAddress(), placeTable[key]);
+                placeTable[key] = NULL;
+                return ht->addPlace(address, place);
+            } else if (hashTable[key] != NULL) {
+                return hashTable[key]->addPlace(address, place);
+            }
+
+            placeTable[key] = place;
+            return this;
+        }
+        Node<Place>* tableToNodes(Node<Place>* lastNode) {
+            for (int i = 0; i < 28; i += 1) {
+                Node<Place>* tmp;
+                if (hashTable[i] != NULL) {
+                    tmp = hashTable[i]->tableToNodes(lastNode);
+                    if (lastNode != NULL) {
+                        lastNode->setNext(tmp);
+                    }
+                    lastNode = tmp;
+                } else if (placeTable[i] != NULL) {
+                    tmp = new Node<Place>(placeTable[i]);
+                    if (lastNode != NULL) {
+                        lastNode->setNext(tmp);
+                    }
+                    lastNode = tmp;
+                }
+            }
+
+            return lastNode;
+        }
+    public:
+        HashTable(int nIndex = 0) {
+            index = nIndex;
+        }
+        HashTable* add(Place* place) {
+            return addPlace(place->getAddress(), place);
+        }
+        HashTable* remove(string name) {
+            return this;
+        }
+        Node<Place>* get(string name) {
+            int key = hash(name);
+
+            // Get from hashtable.
+            if (hashTable[key] != NULL) {
+                if (name[index] != NUL) {
+                    return hashTable[key]->get(name);
+                } else {
+                    return hashTable[key]->tableToNodes(NULL);
+                }
+            }
+
+            return new Node<Place>(placeTable[key]);
+        }
+};
+
+int main() {
+    HashTable ht;
+
+    Place* place1 = new Place("earth", 0.0, 0.0);
+    Place* place2 = new Place("uk", 0.0, 0.0);
+    Place* place3 = new Place("birmingham", 0.0, 0.0);
+    Place* place4 = new Place("us", 0.0, 0.0);
+
+    place1->addChild(place2);
+    place2->addChild(place3);
+    place1->addChild(place4);
+
+    cout << ht.add(place1) << (char)0x0A;
+    cout << ht.add(place2) << (char)0x0A;
+    cout << ht.add(place3) << (char)0x0A;
+    cout << ht.add(place4) << (char)0x0A;
+
+    cout << ht.get("u")->getData()->getAddress() << (char)0x0A;
+
+    return 0;
+}
+
+#endif
