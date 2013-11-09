934efd0ceed621d721acbf359a37d80ac8c5f1d5
Ryan Smith <0ryansmith1994@gmail.com>
2013-11-09 13:16:16 +0000

    Not sure why this doesn't work when I just get "birmingham"

--- src/HashTable.cpp
+++ src/HashTable.cpp
@@ -52,24 +52,30 @@ class HashTable {
             return this;
         }
         Node<Place>* tableToNodes(Node<Place>* lastNode) {
+            Node<Place>* firstNode = lastNode;
+
             for (int i = 0; i < 28; i += 1) {
                 Node<Place>* tmp;
                 if (hashTable[i] != NULL) {
                     tmp = hashTable[i]->tableToNodes(lastNode);
                     if (lastNode != NULL) {
                         lastNode->setNext(tmp);
+                    } else {
+                        firstNode = tmp;
                     }
                     lastNode = tmp;
                 } else if (placeTable[i] != NULL) {
                     tmp = new Node<Place>(placeTable[i]);
                     if (lastNode != NULL) {
                         lastNode->setNext(tmp);
+                    } else {
+                        firstNode = tmp;
                     }
                     lastNode = tmp;
                 }
             }
 
-            return lastNode;
+            return firstNode;
         }
     public:
         HashTable(int nIndex = 0) {
@@ -83,10 +89,11 @@ class HashTable {
         }
         Node<Place>* get(string name) {
             int key = hash(name);
+            
 
             // Get from hashtable.
             if (hashTable[key] != NULL) {
-                if (name[index] != NUL) {
+                if (name[index + 1] != NUL) {
                     return hashTable[key]->get(name);
                 } else {
                     return hashTable[key]->tableToNodes(NULL);
@@ -104,17 +111,27 @@ int main() {
     Place* place2 = new Place("uk", 0.0, 0.0);
     Place* place3 = new Place("birmingham", 0.0, 0.0);
     Place* place4 = new Place("us", 0.0, 0.0);
+    Place* place5 = new Place("alabama", 0.0, 0.0);
+    Place* place6 = new Place("birmingham", 0.0, 0.0);
 
     place1->addChild(place2);
     place2->addChild(place3);
     place1->addChild(place4);
+    place4->addChild(place5);
+    place5->addChild(place6);
 
     cout << ht.add(place1) << (char)0x0A;
     cout << ht.add(place2) << (char)0x0A;
     cout << ht.add(place3) << (char)0x0A;
     cout << ht.add(place4) << (char)0x0A;
+    cout << ht.add(place5) << (char)0x0A;
+    cout << ht.add(place6) << (char)0x0A;
 
-    cout << ht.get("u")->getData()->getAddress() << (char)0x0A;
+    Node<Place>* p = ht.get("birmingham");
+    while (p != NULL) {
+        cout << p->getData()->getAddress() << (char)0x0A;
+        p = p->getNext();
+    }
 
     return 0;
 }
