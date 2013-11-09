4ee121bfde52b71b7ede34d93067f3c10d2670fe
Ryan Smith <0ryansmith1994@gmail.com>
2013-11-09 19:25:38 +0000

    Included tests for HashTable (causes segmentation fault).

--- test/main.cpp
+++ test/main.cpp
@@ -1,11 +1,12 @@
 #include "igloo/igloo_alt.h"
-// #include "DistanceApp.cpp"
+//#include "DistanceApp.cpp"
 #include "DistanceView.cpp"
 #include "LList.cpp"
 #include "Node.cpp"
 #include "OptionsView.cpp"
 #include "OptionsViewPlaces.cpp"
 #include "Place.cpp"
+#include "HashTable.cpp"
 using namespace igloo;
 
 int main(int argc, const char *argv[]) {
