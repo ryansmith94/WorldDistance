63b7b7603f5eee39daf177c93642d81baa5d21ff
H. <harleybristowabc@googlemail.com>
2013-11-13 11:52:42 +0000

    Update on new code

--- src/AddPlacesView.cpp
+++ src/AddPlacesView.cpp
@@ -5,24 +5,38 @@
 using namespace std;
 
 class AddPlacesView: public View {
+private:
+    float inputFloat(String out){
+        float selected;
+        string input;
+        
+        do {
+            cout << "<\t" << out << ": ";
+            getline(cin, input);
+            stringstream myStream(input);
+        } while (!(myStream >> selected));
+        
+        return selected;
+    }
+    
 public:
-    AddPlacesView(Place* place) : View(place) {}
+    AddPlacesView(Place* place, HashTable* hTable) : View(place) {}
     AddPlacesView* start() {
         string sInput;
-        string slInput1, slInput2;
         float lInput1, lInput2;
 
         cout << "Adding a child to " << rootPlace->getName() << "..." << endl;
 
         cout << endl << "New child name:\t ";
-        getline(cin, sInput);
+        sInput = getAddress();
 
-        cout << endl << "New child longitude:\t ";
-        cin >> lInput1;
-        cout << endl << "New child latitude:\t ";
-        cin >> lInput2;
+        lInput1 = inputFloat("New child longitude");
+        lInput2 = inputFloat("New child latitude");
 
-        rootPlace->addChild(new Place(sInput, lInput1, lInput2));
+        Place* p = new Place(sInput, lInput1, lInput2);
+
+        rootPlace->addChild(p);
+        hTable->add(p);
         return this;
     }
 };
