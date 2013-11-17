9918bccca853212e42b9b1603a35399b61e4358e
H. Bristow <harleybristowabc@googlemail.com>
2013-11-17 23:14:47 +0000

    ModifyPlaceView
    Testing and working, Ollie watched me rename Earth to Badingus

--- src/ModifyPlaceView.cpp
+++ src/ModifyPlaceView.cpp
@@ -6,5 +6,35 @@ using namespace std;
 
 ModifyPlacesView::ModifyPlacesView(Place* place, HashTable* hTable) : View(place, hTable) {}
 ModifyPlacesView* ModifyPlacesView::start() {
-    return this;
-}
+        int menuSelection;
+
+        cout << endl << "Modifying place: " << rootPlace->getName() << endl;
+
+        cout << "Edit\nName\t\t\t1\nLongitude\t\t2\nLatitude\t\t3\n\n  > ";
+        cin >> menuSelection;
+
+        if (menuSelection == 1) {
+            cout << "New Name: ";
+            string newString;
+            cin >> newString;
+            rootPlace->setName(newString);
+            
+        } else if (menuSelection == 2) {
+            cout << "New Longitude: ";
+            float newFloat;
+            cin >> newFloat;
+            rootPlace->setLongitude(newFloat);
+            
+        } else if (menuSelection == 3) {
+            cout << "New Latitude: ";
+            float newFloat;
+            cin >> newFloat;
+            rootPlace->setLatitude(newFloat);
+            
+        } else {
+            cout << "Bad selection. Returning to menu."<<endl;
+            
+        }
+
+        return this;
+    }
